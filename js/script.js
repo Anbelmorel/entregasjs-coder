cb630410f73b4eded8937c39b0e06d58dc0e5e06
Anbelmorel <belen.morel3006@gmail.com>
2024-08-07 23:47:31 -0300

    mudansa de computador

--- js/script.js
+++ js/script.js
@@ -1,200 +1,134 @@
-//parte 1 bienvenida
+// declaracion de elementos del body 
+const header = document.createElement("header");
+document.body.appendChild(header);
 
-let nombre = prompt("ingrese su nombre ");
-let apellido = prompt("igrese su apellido");
-alert("hola "+ nombre+ " "  +apellido + " bienvenida/o a a la estetica");
-//parte 2 eleccion de servicios
-let unas = [
-  { nombre: "semipermanente", precio: 5000 },
-  { nombre: "caping", precio: 7000 },
-  { nombre: "esculpidas", precio: 14000 },
-  { nombre: "sof gel", precio: 12000 },
-];
+const main = document.createElement("main");
+document.body.appendChild(main);
 
-let pestanas = [
-  { nombre: "lifting", precio: 5000 },
-  { nombre: "clasicas", precio: 10000 },
-  { nombre: "volumen medio", precio: 12000 },
-  { nombre: "volumen ruso", precio: 14000 },
-  { nombre: "mega volumen", precio: 20000 },
-  { nombre: "efecto cat eyes", precio: 14000 },
-];
-let faciales = [
-  { nombre: "limpieza de cutis", precio: 6600 },
-  { nombre: "peeling mecanico con punta diamante", precio: 10200 },
-  { nombre: "peeling quimico", precio: 7800 },
-  {nombre: "luminoterapia + limpieza facial",precio: 7000,categoria: "promo",},
-  { nombre: "limpieza profunda con cauterizacion", precio: 12300 },
-];
+//menu 
 
-let depilacion = {
-  lacer: [
-    { nombre: "axila + pierna entera", precio: 5000, categoria: "promo" },
-    { nombre: "bozo + axila", precio: 5000, categoria: "promo" },
-    { nombre: "cuerpo entero", precio: 10000 },
-    { nombre: "pierna entera + cavado", precio: 7000, categoria: "promo" },
-    { nombre: "rostro", precio: 3500 },
-  ],
-  cera: [
-    { nombre: "cejas ", precio: 5000 },
-    { nombre: "pierna entera ", precio: 10000 },
-  ],
-};
-// funcion proncipal para la celeccion de servicios 
-function seleccionarServicio() {
+let links = [
+  {nombre:"Servicios", linkText: "../pages/servicios.html"},
+  {nombre:"Inicio", linkTex:"../index.html"}
+  ];
 
-  let continuar = true;
+  function crearMenu (links) {
+    let nav = document.createElement("nav");
+    let ul = document.createElement("ul");
 
-  while (continuar) {
-    
-    let eleccion = prompt(
-      "Seleccione un servicio (uñas, pestañas, faciales, depilacion):"
-    );
-    if (eleccion === "uñas") {
-      return uñas();
-    } else if (eleccion === "pestañas") {
-      console.log("los servicios disponibles son : ");
-      return comunes2();
-    } else if (eleccion === "faciales") {
-      console.log("los servicios disponibles son :  ");
-         comunes();
-    } else if (eleccion === "depilacion") {
-      return depi();
-    } else {
-      console.log("El servicio no se encuentra disponible.");
-    }
-
-    let servi2 = prompt("¿Desea realizarse otro servicio? (si/no):");
-    let continuar = servi2 === "si";
-  }
-}
-seleccionarServicio();
-
-//funcion depi 
-function depi() {
-  let eleccion = prompt(
-    "su eleccion fue depilacion , porfavor eliga entre cera/lacer"
-  );
-  if (eleccion === "lacer") {
-    mostrarServicios(depilacion.lacer);
-  } else if (eleccion === "cera") {
-    mostrarServicios(depilacion.cera);
-  } else {
-    console.log(
-      " porfavor ingrese un servicio de depilacion como cera / lacer"
-    );
-  }
-}
-//funcion uñas 
-function uñas() {
-  mostrarServicios(unas);
-
-  let eleccion = prompt(
-    "Seleccione uno de los servicios de uñas mostrados en consola :"
-  );
-  let servicioSeleccionado = obtenerServicioSeleccionado(unas, eleccion);
-
-  if (!servicioSeleccionado) {
-    console.log("Servicio no encontrado. Por favor, elija un servicio válido.");
-    return;
-  }
-
-  let precioFinal = servicioSeleccionado.precio;
-
-  if (eleccion === "semipermanente" || eleccion === "caping") {
-    let color = prompt("Ingrese el color deseado:");
-    console.log(
-      `Su servicio es ${servicioSeleccionado.nombre} con color ${color} a un precio de ${precioFinal}`
-    );
-  } else if (eleccion === "sof gel" || eleccion === "esculpidas") {
-    let forma = prompt(
-      "Ingrese la forma deseada (almendra, cuadrada, coffin, redonda):"
-    );
-    let tamano = prompt(
-      "Ingrese el tamaño (s=2mm, m=3mm, l=4mm, xl=2cm, xxl=4cm):"
-    ).toLowerCase();
-
-    if (tamano === "xl" || tamano === "xxl") {
-      precioFinal += 5000;
-      console.log(
-        "en esta opcion se le incrementara 5000 pesos mas al precio original , si no le conviene esta opcion , puede elegir entre los talles s. m. l "
-      );
-    }
-
-    console.log(
-      `Su servicio es ${servicioSeleccionado.nombre} con forma ${forma}, tamaño ${tamano} a un precio final de ${precioFinal}`
-    );
-  } else {
-    console.log(
-      "Por favor, elija entre las opciones que se muestran por consola."
-    );
-  }
-}
-
-
-
-// funcion facial 
-function comunes() {
-    mostrarServicios(faciales);
-
-    let eleccion = prompt("Por favor ingrese las opciones que se muestran en consola:").toLowerCase();
-    let servicioSeleccionado = obtenerServicioSeleccionado(faciales, eleccion);
-
-    if (servicioSeleccionado) {
-        let precioFinal = servicioSeleccionado.precio;
-        console.log(`Su servicio es ${servicioSeleccionado.nombre}, a un precio de ${precioFinal}`);
-    } else {
-        console.log("El servicio seleccionado no se encuentra disponible. Por favor, elija un servicio válido.");
-    }
-}
-//funcion pestañas 
-function comunes2() {
-    mostrarServicios(pestanas);
-
-    let eleccion = prompt("Por favor ingrese las opciones que se muestran en consola:").toLowerCase();
-    let servicioSeleccionado = obtenerServicioSeleccionado(pestanas, eleccion);
-
-    if (servicioSeleccionado) {
-        let precioFinal = servicioSeleccionado.precio;
-        console.log(`Su servicio es ${servicioSeleccionado.nombre}, a un precio de ${precioFinal}`);
-    } else {
-        console.log("El servicio seleccionado no se encuentra disponible. Por favor, elija un servicio válido.");
-    }
-}
-
-
-
-// cosas que se repiten en el codigo 
-function mostrarServicios(servicios) {
-    console.log("los servicios disponibles son :");
-    servicios.forEach((servicio) =>
-      console.log(`${servicio.nombre} - ${servicio.precio}`)
-    );
-  }
-  
-  function obtenerServicioSeleccionado(servicios, eleccion) {
-    return servicios.find(
-      (servicio) => servicio.nombre.toLowerCase() === eleccion
-    );
-  }
-//psrte 3 agenda el turno segunda entrega agregar date
-function turnos (){ 
-    let dia = prompt("ingrese el dia a realizarse el servicio");
-    if ((dia=="lunes")||(dia=="jueves")||(dia == "sabado")) {
-        alert("te esperamos")
+    links.forEach(link => {
+        let li = document.createElement("li");
+        let a = document.createElement("a");
+        a.href = link.linkText;  // Establece el href con el valor de linkText
+        a.textContent = link.nombre;  // Establece el texto del enlace con el valor de nombre
         
-    } else {
-        alert("agenda llena ,porfavon ingresaotro dia ");
-    }
-}
-turnos ();
+        li.appendChild(a);
+        ul.appendChild(li);
+    });
 
-//parte 4; pagos 
-let formaPago = prompt("ingrese la forma de pago 1 para efectivo , 2 para tarjeta");
-let pago = prompt("ingrese 1 para pago total 2 para seña del servicio");
-
-if (formaPago == 1 && pago == 1) {
-  alert("tenes un 15% de descuento");
-} else {
- alert("gracias por confiar en nuestros servicios");
+    nav.appendChild(ul);
+    return nav;
 }
+
+let menu = crearMenu(links);
+header.appendChild(menu);
+// ver carrucel 
+/*let imagenes = [
+  { ruta: "../imag/imgCarru1.png" },
+  { ruta: "../imag/imgCarru2.png" },
+  { ruta: "../imag/imgCarru3.png" }
+];
+imagenes.forEach((datosimg) => {
+  let img = document.createElement("img");
+  img.src = datosimg.ruta;
+  img.classList.add("imgEstiloCarru");
+  div.appendChild(img);
+});*/
+
+// bienvenida y descripcion de la app 
+let titulo = document.createElement("h1")
+titulo.textContent ="bienvenidos a nuestra estetica"
+titulo.classList.add("titulo")
+main.appendChild(titulo)
+
+let p = document.createElement("p")
+p.classList.add ("descripcion")
+p.textContent= "En nuestra estética, nos dedicamos a realzar tu belleza y bienestar. Ofrecemos una amplia gama de servicios personalizados que incluyen tratamientos faciales, manicuras, extensiones de pestañas, depilación láser y mucho más . Nuestro equipo de profesionales está aquí para brindarte la mejor experiencia posible, utilizando productos de alta calidad y las técnicas más avanzadas del mercado. ¡Te esperamos para que te relajes y te sientas más hermosa que nunca!"
+main.appendChild(p);
+
+//tarjetas estaticas 
+let servicios = [
+  {nombre: "faciales " ,imagenT: "imag/facial.png", descripcion: "Nuestro servicio de limpieza y cuidado de la piel está diseñado para revitalizar y mantener tu piel en óptimas condiciones. Incluye una limpieza profunda para eliminar impurezas, exfoliación para suavizar y renovar la piel, y una serie de tratamientos hidratantes y nutritivos. Adaptamos cada sesión a tus necesidades específicas, asegurando que tu piel luzca fresca, radiante y saludable"},
+  {nombre: "manicuria" ,imagenT: "imag/manucuria.png" , descripcion: "En nuestra estética, ofrecemos un servicio de manicura integral que se adapta a tus necesidades y preferencias. Nuestro equipo de expertos está capacitado en las últimas técnicas y tendencias para garantizar resultados impecables. los distintos servicios que ofrecemos son : capping , semipermanente , sofgel , esculpidas . Cada una de estas técnicas está diseñada para realzar la belleza de tus manos y ofrecerte una experiencia de manicura que se ajuste a tus expectativas. Ven a disfrutar de un servicio de manicura profesional y sal con unas uñas perfectamente cuidadas y bellas"},
+  {nombre: "extenciones de pestañas" , imagenT: "imag/pestanas.png", descripcion: "Transforma tu mirada con nuestro exclusivo servicio de extensiones de pestañas. Ofrecemos una variedad de técnicas para adaptarnos a tus preferencias y estilo: lifting , clasicas , volmen medio , volumen ruso . Cada uno de estos servicios está diseñado para realzar tu belleza natural y ofrecerte un look personalizado que se ajuste a tus gustos y necesidades. Ven y disfruta de una experiencia de extensión de pestañas que te hará sentir y lucir espectacular."},
+  {nombre: "depilacion" ,imagenT: "imag/depi.png", descripcion: "La depilación láser es una solución eficaz y duradera para eliminar el vello no deseado. Utilizamos tecnología avanzada que actúa directamente sobre el folículo piloso, reduciendo el vello de manera progresiva y minimizando la posibilidad de crecimiento futuro. Ideal para lograr una piel suave y libre de vello en áreas como piernas, axilas, y más."}
+
+]
+let conteTarjetas = document.createElement("div")
+conteTarjetas.classList.add("tarjetasEst")
+main.appendChild(conteTarjetas);
+
+servicios.forEach(servicio => {
+  let tarjeta = document.createElement("div");
+  tarjeta.className = "tarjeta";
+
+  let imagen = document.createElement("img");
+  imagen.src = servicio.imagenT;
+  imagen.alt = servicio.nombre;
+  imagen.className = "tarjeta-imagen";
+  tarjeta.appendChild(imagen);
+
+  let titulo = document.createElement("h3");
+  titulo.textContent = servicio.nombre;
+  tarjeta.appendChild(titulo);
+
+  let descripcion = document.createElement("p");
+  descripcion.textContent = servicio.descripcion;
+  tarjeta.appendChild(descripcion);
+
+  conteTarjetas.appendChild(tarjeta);
+});
+// direccion a nuestra aplicacion web 
+let titulo2 = document.createElement("h2")
+titulo2.textContent= "solicitar servicios"
+let descripcion2 = document.createElement("p")
+descripcion2.textContent= "Para solicitar nuestros servicios, visita la sección correspondiente en el enlace proporcionado o dirígete directamente a la sección de servicios para obtener más información."
+main.appendChild(titulo2)
+main.appendChild(descripcion2)
+let a = document.createElement("a")
+a.href = "pages/servicios.html"
+a.classList.add ("enlace")
+a.textContent ="ver servicios"
+main.appendChild(a)
+
+//  footer
+let footer = document.createElement("footer");
+document.body.appendChild(footer);
+
+let redes = [
+  {nombre: "instagram ", imagenF:"imag/instagram.png"},
+  {nombre: " facebook ", imagenF:"imag/facebook.png"},
+  {nombre: " whatsapp ", imagenF:"imag/whatsapp.png"}
+]
+
+function iconos(redes) {
+  let conteFooter = document.createElement("div");
+  conteFooter.classList.add("contFooter"); 
+
+  redes.forEach((icono) => {
+    let img = document.createElement("img");
+    img.src = icono.imagenF;
+    img.alt = icono.nombre; 
+    img.classList.add("imgfooter");
+    conteFooter.appendChild(img);
+  });
+
+  return conteFooter; 
+}
+
+const footerIcons = iconos(redes); 
+footer.appendChild(footerIcons); 
+
+let  derechos = document.createElement("p");
+derechos.textContent = "© 2024 Estética. Todos los derechos reservados.";
+footer.appendChild(derechos);
---
+++ js/script2.js
@@ -0,0 +1,30 @@
+let unas = [
+    { nombre: "semipermanente", precio: 5000 },
+    { nombre: "caping", precio: 7000 },
+    { nombre: "esculpidas", precio: 14000 },
+    { nombre: "sof gel", precio: 12000 },
+];
+
+  let pestanas = [
+    { nombre: "lifting", precio: 5000 },
+    { nombre: "clasicas", precio: 10000 },
+    { nombre: "volumen medio", precio: 12000 },
+    { nombre: "volumen ruso", precio: 14000 },
+    { nombre: "mega volumen", precio: 20000 },
+    { nombre: "efecto cat eyes", precio: 14000 },
+];
+  let faciales = [
+    { nombre: "limpieza de cutis", precio: 6600 },
+    { nombre: "peeling mecanico con punta diamante", precio: 10200 },
+    { nombre: "peeling quimico", precio: 7800 },
+    {nombre: "luminoterapia + limpieza facial",precio: 7000,categoria: "promo",},
+    { nombre: "limpieza profunda con cauterizacion", precio: 12300 },
+];
+  
+let depilacionlaser =  [
+    { nombre: "axila + pierna entera", precio: 5000, categoria: "promo" },
+    { nombre: "bozo + axila", precio: 5000, categoria: "promo" },
+    { nombre: "cuerpo entero", precio: 10000 },
+    { nombre: "pierna entera + cavado", precio: 7000, categoria: "promo" },
+    { nombre: "rostro", precio: 3500 },
+];
